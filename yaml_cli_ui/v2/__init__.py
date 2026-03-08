7eb002459a062a0b56e5efc3dd4aa97777f9df6f
PhysicistLeon <physicistleon@gmail.com>
2026-03-09 01:36:41 +0300

    Fix v2 package exports to satisfy Ruff F401

--- yaml_cli_ui/v2/__init__.py
+++ yaml_cli_ui/v2/__init__.py
@@ -1,25 +1,15 @@
 """Public API surface for YAML CLI UI v2 scaffold."""
 
+from . import models as _models
 from .loader import load_v2_document, load_yaml_file, resolve_imports
-from .models import (
-    PUBLIC_API_MODELS,
-    CommandDef,
-    ErrorContext,
-    ForeachSpec,
-    LauncherDef,
-    ParamDef,
-    ParamType,
-    PipelineDef,
-    ProfileDef,
-    RunContext,
-    RunSpec,
-    SecretSource,
-    StepResult,
-    StepSpec,
-    StepStatus,
-    V2Document,
-)
 from .validator import validate_v2_document
+
+# Re-export selected model symbols as part of package public API.
+PUBLIC_API_MODELS = _models.PUBLIC_API_MODELS
+for _name in PUBLIC_API_MODELS:
+    globals()[_name] = getattr(_models, _name)
+
+del _name
 
 __all__ = [
     *PUBLIC_API_MODELS,
