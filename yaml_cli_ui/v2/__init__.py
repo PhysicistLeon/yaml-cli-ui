71a0f435b747e1ef45a9e6427d1c4f28c718ccca
PhysicistLeon <physicistleon@gmail.com>
2026-03-09 01:39:54 +0300

    Make v2 exports pylint-friendly and explicit

--- yaml_cli_ui/v2/__init__.py
+++ yaml_cli_ui/v2/__init__.py
@@ -1,18 +1,59 @@
 """Public API surface for YAML CLI UI v2 scaffold."""
 
-from . import models as _models
 from .loader import load_v2_document, load_yaml_file, resolve_imports
+from .models import (
+    CommandDef as CommandDef,
+    ErrorContext as ErrorContext,
+    ForeachSpec as ForeachSpec,
+    LauncherDef as LauncherDef,
+    ParamDef as ParamDef,
+    ParamType as ParamType,
+    PipelineDef as PipelineDef,
+    ProfileDef as ProfileDef,
+    RunContext as RunContext,
+    RunSpec as RunSpec,
+    SecretSource as SecretSource,
+    StepResult as StepResult,
+    StepSpec as StepSpec,
+    StepStatus as StepStatus,
+    V2Document as V2Document,
+)
 from .validator import validate_v2_document
 
-# Re-export selected model symbols as part of package public API.
-PUBLIC_API_MODELS = _models.PUBLIC_API_MODELS
-for _name in PUBLIC_API_MODELS:
-    globals()[_name] = getattr(_models, _name)
-
-del _name
+PUBLIC_API_MODELS = (
+    "CommandDef",
+    "ErrorContext",
+    "ForeachSpec",
+    "LauncherDef",
+    "ParamDef",
+    "ParamType",
+    "PipelineDef",
+    "ProfileDef",
+    "RunContext",
+    "RunSpec",
+    "SecretSource",
+    "StepResult",
+    "StepSpec",
+    "StepStatus",
+    "V2Document",
+)
 
 __all__ = [
-    *PUBLIC_API_MODELS,
+    "CommandDef",
+    "ErrorContext",
+    "ForeachSpec",
+    "LauncherDef",
+    "ParamDef",
+    "ParamType",
+    "PipelineDef",
+    "ProfileDef",
+    "RunContext",
+    "RunSpec",
+    "SecretSource",
+    "StepResult",
+    "StepSpec",
+    "StepStatus",
+    "V2Document",
     "load_yaml_file",
     "resolve_imports",
     "load_v2_document",
